08d034d79444b19d5a301a539926b1c9ecd1c817
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2024-03-19 18:57:29 +0300

    initializ the app

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './db/connect.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+import notFoundMiddleware from './middleware/not-found.js'
+
+// Routes
+import userRoutes from './routers/userRoutes.js'
+// import postRoutes from "./routes/postRoutes";
+// import commentRoutes from "./routes/commentRoutes";
+// import postCategoriesRoutes from "./routes/postCategoriesRoutes";
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('server is running...')
+})
+
+app.use('/api/users', userRoutes)
+// app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/post-categories", postCategoriesRoutes);
+
+// error handler middleware
+// app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
