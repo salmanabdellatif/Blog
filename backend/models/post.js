8432b143cab3b8ffad5d6f1e164df01bf991d1a7
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2024-04-13 12:14:58 +0300

    update photo type Buffer: String

--- backend/models/post.js
+++ backend/models/post.js
@@ -4,9 +4,9 @@ const postSchema = new Schema(
   {
     title: { type: String, required: true },
     caption: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
     content: { type: String, required: true },
-    photo: { type: Buffer, required: [true, 'please provide image'] },
+    slug: { type: String, required: true, unique: true },
+    photo: { type: String, required: true },
     user: { type: Schema.Types.ObjectId, ref: 'user', required: true },
     tags: [{ type: String }],
     categories: [{ type: Schema.Types.ObjectId, ref: 'PostCategory' }],
