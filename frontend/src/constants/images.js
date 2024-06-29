15938765d6a817558a9e4802e3c7aa78c090c883
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2024-06-29 22:48:19 +0300

    Add Images Importer

---
+++ frontend/src/constants/images.js
@@ -0,0 +1,17 @@
+import logo from '../assets/logo.svg'
+import WhiteLogo from '../assets/WhiteLogo.svg'
+import HeroImage from '../assets/HeroImage.svg'
+import Post1Image from '../assets/posts/Post1.jpg'
+import PostProfile from '../assets/posts/PostProfile.svg'
+import CtaImage from '../assets/CtaImage.png'
+
+const images = {
+  logo,
+  WhiteLogo,
+  HeroImage,
+  Post1Image,
+  PostProfile,
+  CtaImage,
+}
+
+export default images
