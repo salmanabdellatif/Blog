d8468ac08914677f1b8924b8a7ab4c296cb4daea
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2024-07-02 00:48:26 +0300

    Update MinLength of the password 8 -> 6

--- backend/models/user.js
+++ backend/models/user.js
@@ -25,7 +25,7 @@ const UserSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, 'Please provide password'],
-      minlength: 8,
+      minlength: 6,
     },
     verified: {
       type: Boolean,
