9f7785b11e2d5016791ee217e825e4e43a2c3229
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2024-07-02 00:32:28 +0300

    Add [login, register] routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import './App.css'
 import HomePage from './pages/home/HomePage'
 import ArticlePage from './pages/article/ArticlePage'
+import RegisterPage from './pages/register/RegisterPage'
+import LoginPage from './pages/login/LoginPage'
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <Routes>
         <Route index path='/' element={<HomePage />} />
         <Route path='/blog/:id' element={<ArticlePage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
+      <Toaster />
     </div>
   )
 }
