094241d98b376c12018a147d81c04d95046a098e
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2024-12-13 02:48:25 +0300

    add functions [getUserProfile, updateProfile, updateProfilePicture]

--- frontend/src/services/index/users.js
+++ frontend/src/services/index/users.js
@@ -9,8 +9,7 @@ export const signup = async ({ name, email, password }) => {
     })
     return data
   } catch (error) {
-    if (error.response && error.response.data.message)
-      throw new Error(error.response.data.message)
+    if (error.response && error.response.data.message) throw new Error(error.response.data.message)
     throw new Error(error.message)
   }
 }
@@ -22,8 +21,53 @@ export const login = async ({ email, password }) => {
     })
     return data
   } catch (error) {
-    if (error.response && error.response.data.message)
-      throw new Error(error.response.data.message)
+    if (error.response && error.response.data.message) throw new Error(error.response.data.message)
+    throw new Error(error.message)
+  }
+}
+
+export const getUserProfile = async ({ token }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    const { data } = await axios.get('/api/users/user', config)
+    return data
+  } catch (error) {
+    if (error.response && error.response.data.message) throw new Error(error.response.data.message)
+    throw new Error(error.message)
+  }
+}
+
+export const updateProfile = async ({ token, userData, userId }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    const { data } = await axios.put(`/api/users/updateUser/${userId}`, userData, config)
+    return data
+  } catch (error) {
+    if (error.response && error.response.data.message) throw new Error(error.response.data.message)
+    throw new Error(error.message)
+  }
+}
+
+export const updateProfilePicture = async ({ token, formData }) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    const { data } = await axios.put(`/api/users/updateUserAvatar`, formData, config)
+    return data
+  } catch (error) {
+    if (error.response && error.response.data.message) throw new Error(error.response.data.message)
     throw new Error(error.message)
   }
 }
